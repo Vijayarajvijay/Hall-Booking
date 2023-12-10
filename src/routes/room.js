7a7d6abed80140c81411ae4094f8f3ca00442442
Vijayrajvijay <vr104555@gmail.com>
2023-12-10 11:49:48 +0530

    messsge update

--- src/routes/room.js
+++ src/routes/room.js
@@ -3,6 +3,10 @@ import roomController from '../controller/room.js'
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.status(200).send(` <h1>Welcome to  Hall Booking project</h1>`);
+  });
+
 router.post('/createRoom',roomController.createRoom)
 router.post('/bookingRoom',roomController.bookRoom)
 router.get('/allBookedRoom',roomController.getRoomsBookedData)
