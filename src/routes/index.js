36bfd9804ff32f178c2cab54ce07b46515f22b0e
Vijayrajvijay <vr104555@gmail.com>
2023-12-11 12:34:49 +0530

    created

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import expres from 'express'
 import roomRouter from './room.js'
 
 const router = expres.Router();
+router.get("/", (req, res) => {
+    res.status(200).send(` <h1>Welcome to  Hall Booking project</h1>`);
+  });
+
 
 router.use('/room',roomRouter)
 
--- src/routes/room.js
+++ src/routes/room.js
@@ -3,9 +3,6 @@ import roomController from '../controller/room.js'
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.status(200).send(` <h1>Welcome to  Hall Booking project</h1>`);
-  });
 
 router.post('/createRoom',roomController.createRoom)
 router.post('/bookingRoom',roomController.bookRoom)
